c9c812fd360190ded684f98a05bacb49349cbf49
marinmihailiviu <68141252+marinmihailiviu@users.noreply.github.com>
2020-08-28 14:20:53 +0300

    28 August - Partea2

---
+++ Week6/TemaSaptamana6/cerinta4.js
@@ -0,0 +1,13 @@
+// 4. Scrieti o functie de tip IIFE care:
+// - sorteaza urmatorul array in functie de proprietatea "salary" a fiecarui obiect continut:
+// var cars = [
+//   {name: "John",  salary: 20000 },
+//   { name: "Danny", salary: 30500 },
+//   { name: "Bekker", salary: 15000 }
+// ];
+// - afiseaza in consola array-ul sortat
+
+// Veti crea un fisier javascript separat pe care il veti include intr-un fisier html. Verificarea se va face prin deschiderea paginii html in browser.
+
+
+
